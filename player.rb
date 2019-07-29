ca5bd680638bc8c32f8d34d086c7816ef6d20b74
Marco Beduschi <beduschimarco@gmail.com>
2019-07-29 08:59:35 -0300

    wip.: transition board.pieces from array of strings to array of squares

--- player.rb
+++ player.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Player
   def initialize(game, marker)
     @game = game
@@ -15,6 +17,7 @@ class Player
 
       if @board.pieces[spot] != @bot.marker && @board.pieces[spot] != @marker
         @board.pieces[spot] = @marker
+        @board.set_quare_value(spot, @marker)
       else
         spot = nil
       end
