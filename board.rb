505c84fb9d4144fab50eeaeea9fbd4571d9003e3
Marco Beduschi <beduschimarco@gmail.com>
2019-07-29 19:39:57 -0300

    Add new helper methods & remove unecessary variables

--- board.rb
+++ board.rb
@@ -1,31 +1,27 @@
 # frozen_string_literal: true
+
 require './square.rb'
 
 class Board
-  attr_reader :pieces, :squares
+  attr_reader :squares
 
   def initialize(game)
     @game = game
-    @pieces = %w[0 1 2 3 4 5 6 7 8]
     @squares = setup_squares
+  end
+
+  def mark_square(square, marker)
+    return unless avaliable_squares.include?(square)
+
+    square.value = marker
   end
 
   def get_square(index)
     squares[index]
   end
 
-  def set_quare_value(index, value)
-    square = squares[index]
-
-    return false unless square.empty?
-
-    square.value = value
-  end
-
   def avaliable_squares
-    squares.map do |square|
-      square if square.empty?
-    end
+    squares.select(&:empty?)
   end
 
   def full?
