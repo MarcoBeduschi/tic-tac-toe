86e7dfaf9804d3474f8974afd8f07f18ee0701d5
Marco Beduschi <beduschimarco@gmail.com>
2019-07-29 09:00:42 -0300

    Add helper methods to Board

--- board.rb
+++ board.rb
@@ -1,9 +1,40 @@
-class Board
+# frozen_string_literal: true
+require './square.rb'
 
-  attr_reader :pieces
+class Board
+  attr_reader :pieces, :squares
 
   def initialize(game)
     @game = game
-    @pieces = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    @pieces = %w[0 1 2 3 4 5 6 7 8]
+    @squares = setup_squares
+  end
+
+  def get_square(index)
+    squares[index]
+  end
+
+  def set_quare_value(index, value)
+    square = squares[index]
+
+    return false unless square.empty?
+
+    square.value = value
+  end
+
+  def avaliable_squares
+    squares.map do |square|
+      square if square.empty?
+    end
+  end
+
+  def full?
+    squares.all? { |s| !s.empty? }
+  end
+
+  private
+
+  def setup_squares
+    Array.new(9).map { Square.new(self) }
   end
 end
