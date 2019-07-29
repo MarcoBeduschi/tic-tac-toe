bafb0cf28243aa165343afd0afe6718ae6127877
Marco Beduschi <beduschimarco@gmail.com>
2019-07-29 08:58:37 -0300

    style fixes

--- view.rb
+++ view.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal: true
+
 class View
   def initialize(game)
     @game = game
-    @board = game.board
   end
 
   def render_game_start
-    puts "Enter [0-8]:"
+    puts 'Enter [0-8]:'
   end
 
   def render_game_over
-    puts "Game Over"
+    puts 'Game Over'
   end
 
   def render_board
-    puts " #{@board.pieces[0]} | #{@board.pieces[1]} | #{@board.pieces[2]}\n===+===+===\n #{@board.pieces[3]} | #{@board.pieces[4]} | #{@board.pieces[5]}\n===+===+===\n #{@board.pieces[6]} | #{@board.pieces[7]} | #{@board.pieces[8]}\n"
+    board = @game.board
+
+    puts " #{board.pieces[0]} | #{board.pieces[1]} | #{board.pieces[2]}\n===+===+===\n #{board.pieces[3]} | #{board.pieces[4]} | #{board.pieces[5]}\n===+===+===\n #{board.pieces[6]} | #{board.pieces[7]} | #{board.pieces[8]}\n"
   end
 
   def get_input
